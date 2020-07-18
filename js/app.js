045dc2d22820f21725c2d8a78a7a7b12c97a8882
Hana <>
2020-07-18 13:31:57 -0700

    [app.js] function to creat computer choice

--- js/app.js
+++ js/app.js
@@ -9,11 +9,6 @@ let unitSeven = document.getElementById("unit-seven");
 let unitEight = document.getElementById("unit-eight");
 let unitNine = document.getElementById("unit-nine");
 
-// callback function for onclick function
-function doSomething() {
-  console.log("i'll figure out what goes here later");
-}
-
 // clickable function
 unitOne.addEventListener("click", doSomething);
 unitTwo.addEventListener("click", doSomething);
@@ -25,9 +20,60 @@ unitSeven.addEventListener("click", doSomething);
 unitEight.addEventListener("click", doSomething);
 unitNine.addEventListener("click", doSomething);
 
+// default choices are null
+let player = {
+  currentMove: null
+};
 
+let computer = {
+  currentMove: null
+};
 
+// function for computer to make choice
+function computerMoves () {
+  let moves = [unitOne, unitTwo, unitThree, unitFour, unitFive, unitSix, unitSeven, unitEight, unitNine];
+  let randomMove = Math.floor(Math.random() * moves.length);
+  computer.currentMove = moves[randomMove]
+  if (computer.currentMove === moves[0]) {
+    // const computerMark = document.createElement("p");
+    document.getElementById("unit-one").textContent = "X";
+    console.log("this is gonna mark unit one");
+  } else if (computer.currentMove === moves[1]) {
+    document.getElementById("unit-two").textContent = "X";
+    console.log("this is gonna mark unit two");
+  } else if (computer.currentMove === moves[2]) {
+    document.getElementById("unit-three").textContent = "X";
+    console.log("this is gonna mark unit three");
+  } else if (computer.currentMove === moves[3]) {
+    document.getElementById("unit-four").textContent = "X";
+    console.log("this is gonna mark unit four");
+  } else if (computer.currentMove === moves[4]) {
+    document.getElementById("unit-five").textContent = "X";
+    console.log("this is gonna mark unit five");
+  } else if (computer.currentMove === moves[5]) {
+    document.getElementById("unit-six").textContent ="X";
+    console.log("this is gonna mark unit six");
+  } else if (computer.currentMove === moves[6]) {
+    document.getElementById("unit-seven").textContent ="X";
+    console.log("this is gonna mark unit seven");
+  } else if (computer.currentMove === moves[7]) {
+    document.getElementById("unit-eight").textContent = "X";
+    console.log("this is gonna mark unit eight");
+  } else if (computer.currentMove === moves[8]) {
+    document.getElementById("unit-nine").textContent = "X";
+    console.log("this is gonna mark unit nine");
+  } else {
+    console.log("this is not a box");
+  }
+  // creating the visual move
+}
 
+computerMoves();
+
+// callback function for onclick function
+function doSomething() {
+  console.log("i'll figure out what goes here later");
+}
 
 // function to check if user has won
 function userMoves(playerMove) {
@@ -58,6 +104,7 @@ function userMoves(playerMove) {
 
 }
 
+// userMoves(player.currentMove); ???
 userMoves(unitThree, unitOne, unitEight, unitNine);
 
 // use % to count clicks and alternate between X and 0
