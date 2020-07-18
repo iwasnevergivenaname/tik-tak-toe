1fa291ecfa1d1807c9e828ae9e14d770450a06b3
Hana <>
2020-07-17 22:50:58 -0700

    [index html style.css app.js] added beginning code for creating the grid pattern, styling, and inital JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello frontend');
+let unitOne = document.getElementById("unit-one");
+let unitTwo = document.getElementById("unit-two");
+let unitThree = document.getElementById("unit-three");
+let unitFour = document.getElementById("unit-four");
+let unitFive =  document.getElementById("unit-five");
+let unitSix = document.getElementById("unit-six");
+let unitSeven = document.getElementById("unit-seven");
+let unitEight = document.getElementById("unit-eight");
+let unitNine = document.getElementById("unit-nine");
+
+function userMove() {
+  console.log("i'll figure out what goes here later")
+}
+
+
+
+
+
+// dom
+document.addEventListener("DOMContentLoaded", function() => {
+
+
+
+
+
+
+});
